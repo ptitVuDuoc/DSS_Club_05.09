28af62a136a587419bcc6c2f1a736ffa83e50962
admin <vuvanduoc95@gmail.com>
2018-05-05 17:50:35 +0700

    5/5/2018

--- app/src/main/java/com/example/admin/dss_project/activity/MainAppActivity.java
+++ app/src/main/java/com/example/admin/dss_project/activity/MainAppActivity.java
@@ -1,9 +1,17 @@
 package com.example.admin.dss_project.activity;
 
+import android.support.annotation.IdRes;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.admin.dss_project.R;
+import com.example.admin.dss_project.fragment.BaseFragment;
+import com.example.admin.dss_project.fragment.LoginFragment;
+import com.example.admin.dss_project.fragment.ScanFragment;
+import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.OnTabSelectListener;
 
 public class MainAppActivity extends AppCompatActivity {
 
@@ -11,5 +19,37 @@ public class MainAppActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_app);
+        init();
+    }
+
+    private void init() {
+        addControl();
+        addEvent();
+    }
+
+    private void addControl() {
+        BottomBar bottomBar = (BottomBar) findViewById(R.id.bottomBar);
+    }
+
+    public void addFragment(BaseFragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.setCustomAnimations(R.anim.enter_from_right, R.anim.exit_to_left, R.anim.enter_from_left, R.anim.exit_to_right);
+        fragmentTransaction.replace(R.id.container_main_app, fragment, LoginFragment.class.getSimpleName());
+        fragmentTransaction.addToBackStack(LoginFragment.class.getSimpleName());
+        fragmentTransaction.commit();
+    }
+
+    private void addEvent() {
+        BottomBar bottomBar = (BottomBar) findViewById(R.id.bottomBar);
+        bottomBar.setOnTabSelectListener(new OnTabSelectListener() {
+            @Override
+            public void onTabSelected(@IdRes int tabId) {
+                if (tabId == R.id.tab_scan) {
+                    ScanFragment scanFragment = new ScanFragment();
+                    addFragment(scanFragment);
+                }
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/admin/dss_project/fragment/HistoryFragment.java
@@ -0,0 +1,35 @@
+package com.example.admin.dss_project.fragment;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.admin.dss_project.R;
+import com.example.admin.dss_project.fragment.BaseFragment;
+
+
+public class HistoryFragment extends BaseFragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_history, container, false);
+    }
+
+    @Override
+    protected void initViews() {
+        addControl();
+        addEvent();
+    }
+
+    private void addEvent() {
+    }
+
+    private void addControl() {
+    }
+}
